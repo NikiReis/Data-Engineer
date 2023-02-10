00649a75f9b83033feb57c01576468aa9ea27c5c
Linek Reis <linekreis@hotmail.com>
2023-02-10 08:43:19 -0300

    Update Script.sh

--- atividades/Modulo-9/Script.sh
+++ atividades/Modulo-9/Script.sh
@@ -8,7 +8,7 @@ sudo apt-get -y install postgresql
 
 
 echo "Downloading and installing the PGAdmin"
-Install the public key for the repository (if not done previously):
+# Install the public key for the repository (if not done previously):
 curl -fsS https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo gpg --dearmor -o /usr/share/keyrings/packages-pgadmin-org.gpg
 
 # Create the repository configuration file:
