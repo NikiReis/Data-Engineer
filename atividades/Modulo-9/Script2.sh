0b4d8f8232e25365cda3e57c90a4e81b8df7d075
Linek Reis <linekreis@hotmail.com>
2023-02-10 09:11:58 -0300

    Create Script2.sh
    Creating a new bash script to eventual uninstall procedure

---
+++ atividades/Modulo-9/Script2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Script to remove PostgreSQL from Ubuntu operating system
+sudo apt remove postgresql postgresql-contrib
+
+#Unistalling Dependencies Packages
+sudo apt autoremove
+
+# Listing Postgres Packages
+dpkg -l |grep postgres
+
+# Remove Postgres from your system by running the remove purge command
+sudo apt-get --purge postgresql postgresql-15 postgresql-client-common postgresql-common posgresql-contrib
+
+
+# Listing Postgres Packages again, to see if the unintalling was done correctly
+dpkg -l |grep postgres
