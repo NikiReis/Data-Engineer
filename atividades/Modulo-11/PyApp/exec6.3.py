5c803dfed28e23ce5829d5255e0fbb5a127aef35
NikiReis <linekreis@hotmail.com>
2023-02-19 14:08:10 -0300

    Refactor: Changing pieces of the code and renaming variables

--- atividades/Modulo-11/PyApp/exec6.3.py
+++ atividades/Modulo-11/PyApp/exec6.3.py
@@ -1,61 +1,70 @@
 class Television:
 
+    CHANNEL_LIST = [2, 4, 6, 8, 10, 12, 16, 17, 26, 28]
+
     def __init__(self):
-        self.Channels = [2, 4, 6, 8, 10, 12]
-        self.On = False
-        self.Pos = 0
+        self.Channels = Television.CHANNEL_LIST
+        self.isOn = False
+        self.current_channel = 0
 
     def power(self):
-        if self.On:
+        if self.isOn:
             print('\nTurning off the TV')
-            self.On = False
+            self.isOn = False
         else:
             print('\nTurning on the TV')
-            print(f"Channel {self.Channels[self.Pos]}\n")
-            self.On = True
+            self.isOn = True
+            self.display_channel()
 
     def channel_up(self):
-        if self.On:
+        if self.isOn:
             try:
-                self.Pos += 1
-                print(f'\nChannel: {self.Channels[self.Pos]}')
+                self.current_channel += 1
+                self.display_channel()
 
             except IndexError:
-                self.Pos = 0
-                print(f'\nChannel: {self.Channels[self.Pos]}')
+                self.current_channel = 0
+                self.display_channel()
 
         else:
             print('\nTurning on the TV')
-            print(f"Channel {self.Channels[self.Pos]}\n")
-            self.On = True
+            print(f"Channel {self.Channels[self.current_channel]}\n")
+            self.isOn = True
 
     def channel_down(self):
         if self.On:
             try:
-                self.Pos -= 1
-                print(f'\nChannel: {self.Channels[self.Pos]}')
+                self.current_channel -= 1
+                print(f'\nChannel: {self.Channels[self.current_channel]}')
 
             except IndexError:
-                self.Pos = len(self.Channels) - 1
-                print(f'\nChannel: {self.Channels[self.Pos]}')
+                self.current_channel = len(self.Channels) - 1
+                self.display_channel()
 
         else:
             print('\nTurning on the TV')
-            print(f"Channel {self.Channels[self.Pos]}\n")
-            self.On = True
+            self.display_channel()
+            self.isOn = True
+
+    def display_channel(self):
+        print(f"Channel: {self.Channels[self.current_channel]}")
+
+    def run(self):
+        while True:
+            choice = str(input('\nTurn tv on/off [tv]\nUp channel [uc]\nDown channel [dc]\nExit infinity loop [0]')).lower()
+            if choice == 'tv':
+                self.power()
+            elif choice == 'uc':
+                self.channel_up()
+            elif choice == 'dc':
+                self.channel_down()
+            elif choice == 'e':
+                print('Exiting TV...')
+                break
+            else:
+                print('Invalid input, please try again')
 
 
-tel = Television()
+tv = Television()
+tv.run()
 
-while True:
-
-    op = str(input('\nTurn tv on/off [tv]\nUp channel [uc]\nDown channel [dc]\nExit infinity loop [0]')).lower()
-    match op:
-        case 'tv':
-            tel.power()
-        case 'uc':
-            tel.channel_up()
-        case 'dc':
-            tel.channel_down()
-        case '0':
-            break
