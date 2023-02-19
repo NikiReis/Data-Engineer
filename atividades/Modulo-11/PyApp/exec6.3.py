6613aef45ae2890a3f65feb189608d279e9ad611
NikiReis <linekreis@hotmail.com>
2023-02-18 16:18:54 -0800

    Feat: Exploring more possibilities with classe. Simulatign a TV

---
+++ atividades/Modulo-11/PyApp/exec6.3.py
@@ -0,0 +1,59 @@
+class Television:
+
+    def __init__(self):
+        self.Channels = [2, 4, 6, 8, 10, 12]
+        self.On = False
+        self.Pos = 0
+
+    def power(self):
+        if self.On:
+            print('\nTurning off the TV')
+            self.On = False
+        else:
+            print('\nTurning on the TV')
+            print(f"Channel {self.Channels[self.Pos]}\n")
+            self.On = True
+
+    def channel_up(self):
+        if self.On:
+            try:
+                self.Pos += 1
+                print(f'\nChannel: {self.Channels[self.Pos]}')
+
+            except IndexError:
+                self.Pos = 0
+                print(f'\nChannel: {self.Channels[self.Pos]}')
+
+        else:
+            print('\nTurning on the TV')
+            print(f"Channel {self.Channels[self.Pos]}\n")
+            self.On = True
+
+    def channel_down(self):
+        if self.On:
+            try:
+                self.Pos -= 1
+                print(f'\nChannel: {self.Channels[self.Pos]}')
+
+            except IndexError:
+                self.Pos = len(self.Channels) - 1
+                print(f'\nChannel: {self.Channels[self.Pos]}')
+
+        else:
+            print('\nTurning on the TV')
+            print(f"Channel {self.Channels[self.Pos]}\n")
+            self.On = True
+
+
+tel = Television()
+
+while True:
+
+    op = str(input('\nTurn tv on/off [tv]\nUp channel [uc]\nDown channel [dc]:  ')).lower()
+    match op:
+        case 'tv':
+            tel.power()
+        case 'uc':
+            tel.channel_up()
+        case 'dc':
+            tel.channel_down()
