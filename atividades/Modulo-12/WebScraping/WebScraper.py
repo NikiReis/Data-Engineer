415e4b28bc6135ab54dd85f2013108c9d5b89877
Linek Reis <linekreis@hotmail.com>
2023-03-06 00:16:54 -0300

    Update WebScraper.py

--- atividades/Modulo-12/WebScraping/WebScraper.py
+++ atividades/Modulo-12/WebScraping/WebScraper.py
@@ -1,25 +1,28 @@
+import csv
 from bs4 import BeautifulSoup
 import requests
 
-data = requests.get('https://scholar.google.com/scholar?q=python&hl=en&as_sdt=0,5').text
+url = requests.get('https://scholar.google.com/scholar?q=python&hl=en&as_sdt=0,5').text
 
-soup = BeautifulSoup(data, 'html.parser')
+soup = BeautifulSoup(url, 'html.parser')
 lista = soup.find_all('div', class_="gs_ri")
 
-datalist = []
-newdata = {
+book = authors = journal = year = publisher = []
+data = dict()
 
-}
 for i in lista:
+  book.append(i.find("h3", a_='').text.strip().replace('[PDF][PDF]', '').replace('[BOOK][B]', '').replace('[HTML][HTML]', ''))
+  authors.append(i.find("div", class_="gs_a").text.split('-')[0])
+  journal.append(i.find("div", class_="gs_a").text.split('-')[1])
+  year.append(i.find("div", class_="gs_a").text.split("-")[1].strip().split()[0])
+  publisher.append(i.find("div", class_="gs_a").text.split("-")[2].strip().split()[0])
 
-  newdata['book'] = i.find("h3", a_='').text.strip().replace('[PDF][PDF]', '').replace('[BOOK][B]', '').replace('[HTML][HTML]', '')
-  newdata['authors'] = i.find("div", class_="gs_a").text.split('-')[0]
-  newdata['journal'] = i.find("div", class_="gs_a").text.split('-')[1]
-  newdata['year'] = publisher = i.find("div", class_="gs_a").text.split("-")[1].strip().split()[0]
-  newdata['publisher'] = i.find("div", class_="gs_a").text.split("-")[2].strip().split()[0]
+data['Book'] = book
+data['Authors'] = authors
+data['Journal'] = journal
+data['Year'] = year
+data['Publisher'] = publisher
 
-  datalist.append(newdata.copy())
-  newdata.clear()
+print(data)
 
-for x in datalist:
-  print(x)
+# with open('data_set.csv','w',encoding='utf8b') as archive:
