3835a98bf7ca1593d754b372fb23975bdaf0caef
Linek Reis <linekreis@hotmail.com>
2023-03-07 19:13:04 -0300

    Update WebScraper.py

--- atividades/Modulo-12/WebScraping/WebScraper.py
+++ atividades/Modulo-12/WebScraping/WebScraper.py
@@ -2,36 +2,27 @@ import csv
 from bs4 import BeautifulSoup
 import requests
 
-url = requests.get('https://scholar.google.com/scholar?q=python&hl=en&as_sdt=0,5').text
+url = requests.get('https://scholar.google.com/scholar?q=python&hl=en&num=20&as_sdt=0,5').text
 
 soup = BeautifulSoup(url, 'html.parser')
 lista = soup.find_all('div', class_="gs_ri")
 
-book = list()
-authors = list()
-journal = list()
-year = list()
-publisher = list()
+datalist = list()
 data = dict()
 
 for i in lista:
-  book.append(i.find("h3", a_='').text.strip().replace('[PDF][PDF]', '').replace('[BOOK][B]', '').replace('[HTML][HTML]', ''))
-  authors.append(i.find("div", class_="gs_a").text.split('-')[0])
-  journal.append(i.find("div", class_="gs_a").text.split('-')[1])
-  year.append(i.find("div", class_="gs_a").text.split("-")[1].strip().split()[0])
-  publisher.append(i.find("div", class_="gs_a").text.split("-")[2].strip().split()[0])
+  data['Book'] = i.find("h3", a_='').text.strip().replace('[PDF][PDF]', '').replace('[BOOK][B]', '').replace('[HTML][HTML]', '')
+  data['Authors'] = i.find("div", class_="gs_a").text.split('-')[0]
+  data['Journal'] = i.find("div", class_="gs_a").text.split('-')[1]
+  data['Year'] = i.find("div", class_="gs_a").text.split("-")[1].strip().split()[0]
+  data['Publisher'] = i.find("div", class_="gs_a").text.split("-")[2].strip().split()[0]
 
-data['Book'] = book
-data['Authors'] = authors
-data['Journal'] = journal
-data['Year'] = year
-data['Publisher'] = publisher
+  datalist.append(data.copy())
+  data.clear()
 
-
-
-with open('data_set.csv', 'w', newline='') as archive:
-  writer = csv.writer(archive)
-  writer.writerow(data.keys())
-  for iteration in range(len(data.keys())):
-    writer.writerow([value[iteration].strip() for value in data.values()])
+fields = ['Book','Authors','Journal','Year','Publisher']
+with open('data_set.csv', 'a', newline='') as archive:
+  writer = csv.DictWriter(archive, fieldnames=fields)
+  writer.writeheader()
+  writer.writerows(datalist)
 
